7224e1195179aed8aafcae39de6bba0a0eed6a22
lonelykkj <heitormanoel05@gmail.com>
2025-03-06 13:52:03 -0300

    aplicação de teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,13 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
+
 from fast_api.app import app
 
-client = TestClient(app)
 
 def test_read_root_deve_retornar_ok_e_hello_world():
-    ...
+    client = TestClient(app)  # Arrange (ornganização)
+    response = client.get("/")  # Act (ação)
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {"message": "Hello World!"}
